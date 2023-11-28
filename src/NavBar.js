7e147834151a5fc2953d5e9903d55d51cf8d30be
Ashish Thore <ashishthore@outlook.com>
2023-11-28 19:45:08 +0530

    signup

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,74 +1,8 @@
-// import React, { useState } from "react";
-// import logo from ".//public/flowise_logo.png";
-// import "./Navbar.css";
-// import {
-//     Collapse,
-//     Container,
-//     Navbar,
-//     NavbarToggler,
-//     NavbarBrand,
-//     Nav,
-//     NavItem,
-//     Button,
-// } from "reactstrap";
-
-// const NavBar = () => {
-//     const [isOpen, setIsOpen] = useState(false);
-//     const toggle = () => setIsOpen(!isOpen);
-
-//     return (
-//         <div className="nav-container">
-//             <Navbar color="light" light expand="md" container={false}>
-//                 <Container>
-//                     <NavbarBrand>
-//                         <img
-//                             src={logo}
-//                             alt="Your Logo"
-//                             width="180"
-//                             style={{ marginLeft: "-107px" }}
-//                         />
-//                     </NavbarBrand>
-
-//                     <NavbarToggler onClick={toggle} />
-//                     <Collapse isOpen={isOpen} navbar>
-//                         <Nav className="ml-auto" navbar>
-//                             {/* Remove NavLink components */}
-//                         </Nav>
-//                         <Nav className="d-none d-md-block" navbar>
-//                             <NavItem>
-//                                 <Button
-//                                     id="qsLoginBtn"
-//                                     color="primary"
-//                                     className="btn-margin move-right"
-//                                     onClick={() => console.log("Login clicked")}
-//                                 >
-//                                     Log in
-//                                 </Button>
-//                             </NavItem>
-//                         </Nav>
-//                         <Nav className="d-md-none" navbar>
-//                             <NavItem>
-//                                 <Button
-//                                     id="qsLoginBtn"
-//                                     color="primary"
-//                                     block
-//                                     onClick={() => console.log("Login clicked")}
-//                                 >
-//                                     Log in
-//                                 </Button>
-//                             </NavItem>
-//                         </Nav>
-//                     </Collapse>
-//                 </Container>
-//             </Navbar>
-//         </div>
-//     );
-// };
-
-// export default NavBar;
+// Navbar.js
 import React from "react";
-import "./Navbar.css"; // Import the CSS file
+import "./Navbar.css"; // Import the main CSS file
 import logoPath from "./public/flowise_logo.png"; // Adjust the path to your logo
+
 
 const Navbar = () => {
     return (
@@ -82,10 +16,23 @@ const Navbar = () => {
                 />
             </div>
 
-            {/* Login button on the right */}
-            <div className="login-container">
+            {/* Links beside the logo */}
+            <div className="links-container">
+                <a href="/about" className="navbar-link">
+                    About Us
+                </a>
+                <a href="/documentation" className="navbar-link">
+                    Documentation
+                </a>
+            </div>
+
+            {/* Buttons on the right */}
+            <div className="buttons-container">
                 <button className="login-button" onClick={() => console.log("Login clicked")}>
                     Login
+                </button>
+                <button className="signup-button" onClick={() => console.log("Signup clicked")}>
+                    Signup
                 </button>
             </div>
         </div>
