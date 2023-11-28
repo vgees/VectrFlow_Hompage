b62648cb2322e0a8b1d7a98291c80c615f8b36e5
Ashish Thore <ashishthore@outlook.com>
2023-11-28 19:17:48 +0530

    Navbar

---
+++ src/Logo.js
@@ -0,0 +1,8 @@
+// Logo.js
+import React from 'react';
+
+const Logo = () => {
+    return <img src="/public/flowise_logo.png" alt="Flowise Logo" />;
+};
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// import './index.css';
 import App from './App';
+import NavBar from './NavBar';
+import './Navbar.css'
 import reportWebVitals from './reportWebVitals';
-// import NavBar from './NavBar';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    {/* <NavBar /> */}
+    <NavBar />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
