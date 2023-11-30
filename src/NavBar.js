0f5840a1b80c08320c0a21464805215a47ad422d
Ashish Thore <ashishthore@outlook.com>
2023-11-30 19:58:37 +0530

    Drop down on sign in

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,18 +2,15 @@
 import React from "react";
 import "./Navbar.css"; // Import the main CSS file
 import logoPath from "./public/flowise_logo.png"; // Adjust the path to your logo
-
-
+import GoogleIcon from "./public/google.svg"; // Adjust the path to your Google icon
+import GithubIcon from "./public/github.svg"; // Adjust the path to your GitHub icon
+import EmailIcon from "./public/email.svg"; // Adjust the path to your email icon
 const Navbar = () => {
     return (
         <div className="navbar">
             {/* Logo on the left */}
             <div className="logo-container">
-                <img
-                    src={logoPath}
-                    alt="Logo"
-                    className="logo"
-                />
+                <img src={logoPath} alt="Logo" className="logo" />
             </div>
 
             {/* Links beside the logo */}
@@ -28,12 +25,27 @@ const Navbar = () => {
 
             {/* Buttons on the right */}
             <div className="buttons-container">
-                <button className="login-button" onClick={() => console.log("Login clicked")}>
-                    Sign in
-                </button>
-                <button className="signup-button" onClick={() => console.log("Signup clicked")}>
-                    Sign up
-                </button>
+                <div className="dropdown">
+                    <button className="login-button" onClick={() => console.log("Login clicked")}>
+                        Sign in
+                    </button>
+                    <div className="dropdown-content">
+                        <a href="#email">
+                            <img src={EmailIcon} alt="Email Icon" className="icon" />
+                            email
+                        </a>
+                        <a href="#google">
+                            <img src={GoogleIcon} alt="Google Icon" className="icon" />
+                            Google
+                        </a>
+
+                        <a href="#github">
+                            <img src={GithubIcon} alt="GitHub Icon" className="icon" />
+                            GitHub
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
